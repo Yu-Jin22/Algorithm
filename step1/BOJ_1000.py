e76b4f1f492bbd5a18e0d832c47b2f51c9d27afa
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-19 17:02:36 +0900

    Add files via upload

---
+++ step1/BOJ_1000.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+#1000
+a,b = map(int, input().split())
+print(a+b)
+
+
+# In[27]:
+
+
+#런타임 에러 (ValueError)
+a = int(input())
+b = int(input())
+
+print(a+b)
+
