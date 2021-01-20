870aaf6f06365d45218f2c634ae8ec1dd1f42178
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-20 10:00:41 +0900

    Update BOJ_2753.py

--- step2/BOJ_2753.py
+++ step2/BOJ_2753.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[16]:
-
-
 #2753
 
 year = int(input())
@@ -12,4 +6,3 @@ if (year%4 == 0 and year%100 !=0) or (year%400 ==0) :
     print('1')
 else : 
     print('0')
-
