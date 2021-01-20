d0478aebb2693fed7bb9a21df96040129dbed728
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-20 10:49:29 +0900

    Update BOJ_2884.py

--- step2/BOJ_2884.py
+++ step2/BOJ_2884.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[32]:
-
-
 #2884
 h,m = map(int, input().split())
 
@@ -13,10 +7,3 @@ elif m < 45 and h > 0:
     print(h-1,m+15)
 else: 
     print(23,m+15)
-
-
-# In[ ]:
-
-
-
-
