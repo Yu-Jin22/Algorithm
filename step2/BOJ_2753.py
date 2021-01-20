0dddded5a38bb100b81d44a09e0f78c74c4f3788
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-20 10:00:19 +0900

    Add files via upload

---
+++ step2/BOJ_2753.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+#2753
+
+year = int(input())
+
+if (year%4 == 0 and year%100 !=0) or (year%400 ==0) :
+    print('1')
+else : 
+    print('0')
+
