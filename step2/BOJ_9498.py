3f39c126e788f003904cd35aabaef0e8836f561a
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-20 09:50:22 +0900

    Add files via upload

---
+++ step2/BOJ_9498.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#9498
+
+score = int(input())
+
+if 90 <= score <= 100 :
+    print('A')
+elif score >= 80  :
+    print('B')
+elif score >= 70  :
+    print('C')
+elif score >= 60  :
+    print('D')
+else : 
+    print('F')
+
