3b4752fef6b3fe5accf464af17a217392d83c535
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-21 11:30:38 +0900

    Update BOJ_2438_2439.py

--- step3/BOJ_2438_2439.py
+++ step3/BOJ_2438_2439.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
-
-
 #2438 -1
-
 t = int(input())
 
 for i in range(t) : 
     print('*'*(i+1))
 
-
-# In[49]:
-
-
 #2438 -2
 i= 0
 exec("i+=1;print('*'*i);"*int(input()))
 
-
-# In[51]:
 
 
 #2439 -1
@@ -28,10 +15,6 @@ t = int(input())
 
 for i in range(t) : 
     print(" "*(t-i-1) + ('*'*(i+1)))
-
-
-# In[54]:
-
 
 #2439 -2
 i= 0
