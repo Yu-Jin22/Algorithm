29473e0516d286e06dac578ee6f3a8f9e769cabf
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-21 10:58:08 +0900

    Update and rename BOJ11021.py to BOJ_11021.py

--- step3/BOJ_11021.py
+++ step3/BOJ_11021.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[24]:
-
-
 #11021
-
 t = int(input())
 
 for i in range(t) : 
     a,b = map(int, input().split())
     print('Case #%d: %d' %(i+1,a+b))
-
-
-# In[ ]:
-
-
-
-
