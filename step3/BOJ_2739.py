7b9dba47eb0a62df1f6b9645ac0ad482b0b0087e
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-21 10:08:06 +0900

    Update BOJ_2739.py

--- step3/BOJ_2739.py
+++ step3/BOJ_2739.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 #2739 - 1
 n = int(input())
 
@@ -11,18 +5,8 @@ for i in range (1,10) :
     print("%d * %d = %d" %(n,i,n*i))
 
 
-# In[8]:
-
-
 #2739 - 2
 n = int(input())
 
 for i in range (1,10) : 
     print(n,'*',i,'=',n*i)
-
-
-# In[ ]:
-
-
-
-
