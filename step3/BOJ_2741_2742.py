f2581621198a792eb55f495740d5b0a40553c1e6
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-21 10:46:26 +0900

    Add files via upload

---
+++ step3/BOJ_2741_2742.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#2741
+
+n = int(input())
+
+for i in range(1,n+1) : 
+    print(i)
+
+
+# In[11]:
+
+
+#2742
+n = int(input())
+
+for i in range(n) : 
+    print(n-i)
+
