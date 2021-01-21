7064458cec33ed8d1ea3d4c6625de6e8e40982d3
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-21 10:07:26 +0900

    Add files via upload

---
+++ step3/BOJ_2739.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#2739 - 1
+n = int(input())
+
+for i in range (1,10) : 
+    print("%d * %d = %d" %(n,i,n*i))
+
+
+# In[8]:
+
+
+#2739 - 2
+n = int(input())
+
+for i in range (1,10) : 
+    print(n,'*',i,'=',n*i)
+
+
+# In[ ]:
+
+
+
+
