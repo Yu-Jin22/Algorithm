89837237d4d8b11ff6aee9ac4ac6aa03f4079286
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-22 10:11:46 +0900

    Add files via upload

---
+++ step4/BOJ_10951.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#10951 -1
+
+while True :
+    try : 
+        a,b = map(int, input().split())
+        print(a+b)
+    except :
+        break
+
