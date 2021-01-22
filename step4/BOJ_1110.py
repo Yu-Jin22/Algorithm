30511682b0c6d24fb021c48281c04b6ca0a2ea2b
Yu-Jin22 <77143767+Yu-Jin22@users.noreply.github.com>
2021-01-22 10:54:08 +0900

    Add files via upload

---
+++ step4/BOJ_1110.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#1110
+origin = n= int(input()) 
+count = 0
+
+while True : 
+    a = n//10 + n%10 #십의자리수 + 일의자리수
+    new = (n%10)*10 + a%10 #(n의 일의자리수 -> 십의자리수) + a의 일의자리수
+    n = new #update n
+    count +=1 #사이클 길이를 구하기 위해
+    if new == origin :
+        print(count)
+        break
+
